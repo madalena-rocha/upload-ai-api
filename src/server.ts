bc48024547e1c9abb5a1e47a1cb06b8f72251fa5
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-09-13 16:56:25 -0300

    application structure

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import { fastify } from 'fastify'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello World'
-})
+app.register(getAllPromptsRoute)
 
 app.listen({
   port: 3333,
