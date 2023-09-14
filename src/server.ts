c98a760949c0c7cbe90604dc9f1d448e6ba1c213
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-09-14 10:15:45 -0300

    generate title and description of videos

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*', // qualquer url front-end vai poder acessar o back-end
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
   port: 3333,
